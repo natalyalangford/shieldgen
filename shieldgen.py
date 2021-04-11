ba19f19e093f53ec370cd7b1979d393d3fff4351
Natalya Langford <natalyaclangford@gmail.com>
2021-04-10 19:18:25 -0700

    code with me session

--- shieldgen.py
+++ shieldgen.py
@@ -109,7 +109,7 @@ def main():
             sys.exit(-1)
         try:
             with open(args.output, 'w') as fp:
-                json_dict = [{k: v} for k, v in shield_dict.items() for k in ["shemaVersion", "label", "message", "color"]]
+                json_dict = {k: v for k, v in shield_dict.items() for k in ["shemaVersion", "label", "message", "color"]}
                 json.dump(json_dict, fp)
         except OSError as err:
             print(err)
